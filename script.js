47f11aab432cbae1c42d3ea9bea2685d82b23aba
Tumisho Makgeru <makgerutumisho55@gmail.com>
2023-09-24 00:02:02 +0200

    Update 4

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-// JavaScript to toggle the navigation menu
-const navToggle = document.querySelector('.nav-toggle');
-const navList = document.querySelector('#nav-list');
-
-navToggle.addEventListener('click', () => {
-  navList.classList.toggle('active');
-});
+function toggleMenu(){
+    const menu = document.querySelector(".menu-links")
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+}
